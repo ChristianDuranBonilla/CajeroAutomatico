c9948fa172a4ce691c1d215b78ee6aa8e0e9e443
Iván Hernández <90792540+IvanHernandezHe@users.noreply.github.com>
2022-03-26 02:40:26 -0600

    Declaración de arreglo de objetos

---
+++ main.js
@@ -0,0 +1,21 @@
+const Persona = [
+    
+    {
+        id: 0,
+        Nombre: 'Hirumi',
+        contraseña: '12345',
+        saldo: 250
+    },
+    {
+        id: 1,
+        Nombre: 'Hola',
+        contraseña: '777',
+        saldo: 255
+    },
+    {
+        id: 2,
+        Nombre: 'Adios',
+        contraseña: '246',
+        saldo: 550
+    }
+]
